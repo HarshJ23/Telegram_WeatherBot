14f7c4fcacef4d72bfaf95455d837f5eec851455
HarshJ23 <hjawajiwar@gmail.com>
2023-07-12 02:30:00 +0530

    imports fixed2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 const express = require('express');
